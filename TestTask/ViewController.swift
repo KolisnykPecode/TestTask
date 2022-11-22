c8f3df8ff5f6086cb3a536573ede9b87931e7af0
Andrew Kolisnyk <andrew_kolisnyk@pecodesfotware.com>
2022-11-22 12:00:03 +0200

    Initial Commit

---
+++ TestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestTask
+//
+//  Created by kolisnyk9 on 22/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
